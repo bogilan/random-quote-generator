c370283cdb7109d9995afa4b85253e6f93edf39d
bogilan <125219883+bogilan@users.noreply.github.com>
2023-02-12 22:17:10 +0100

    Update script.js
    Added comments

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ const quoteAuthor = document.querySelector('.quote-author');
 const copyBtn = document.querySelector('.quote-copy');
 const shareBtn = document.querySelector('.quote-share');
 
+// generate new quote on page load
 document.addEventListener("DOMContentLoaded", ()=> {
     getQuote();
 });
 
+// New Quote Button calls getQuote function
 generateQuoteBtn.addEventListener('click', ()=> {
     getQuote();
 })
 
+// copy Quote Text into clipboard
 copyBtn.addEventListener('click', ()=> {
     navigator.clipboard.writeText(quoteText.innerHTML);
 })
 
+// share quote text and quote author on Twitter
 shareBtn.addEventListener('click', ()=> {
     let tweetUrl = `https://twitter.com/intent/tweet?text="${quoteText.innerText}" - ${quoteAuthor.innerText}`;
     window.open(tweetUrl);
 })
 
+// getting quote data function
 async function getQuote() {
     try {
-        const res = await fetch('https://api.quotable.io/random');
+        const res = await fetch('https://api.quotable.io/random'); // https://github.com/lukePeavey/quotable API
         const data = await res.json();
-        quoteText.innerText = data.content;
-        quoteAuthor.innerText = data.author;
+        quoteText.innerText = data.content; // get quote content into .quote-text paragraph
+        quoteAuthor.innerText = data.author; // get quote author into .quote-author span
     }
     catch(error) {
         alert('Failed to get new quote!')
